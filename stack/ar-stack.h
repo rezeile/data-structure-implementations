5c188f80e3310b8f9799bd4e2793a868c9a1bfe0
EliezerAbate <code.eliezer@gmail.com>
2015-12-19 12:02:01 -0800

    finished implementation of resizing stack

--- stack/ar-stack.h
+++ stack/ar-stack.h
@@ -9,6 +9,7 @@
 #define _AR_STACK_H
 
 #include <iterator>
+#include <iostream>
 #include <exception>
 
 /* forward declaration to allow use in StackIterator */
@@ -47,18 +48,30 @@ private:
 	T *_items;
 };
 
-/* utility functions */
+/* utility function: resize 
+ * ------------------------
+ * This function resizes the internal array to a new size
+ * specified the variable new_sz.
+ */
 template<typename T>
-void increase_capacity(T *items,int old_sz) {
+void resize(T *&items,int old_sz, int new_sz) {
 	/* copy elements into new array */
-	T *new_items = new int[2 * old_sz];
-	for(int i = 0; i < old_sz; i++) {
+	int sz = old_sz < new_sz ? old_sz : new_sz;
+	T *new_items = new T[new_sz];
+	for(int i = 0; i < sz; i++) {
 		new_items[i] = items[i];
 	}
-	delete[] items;
+	/* why does it say that items was not allocated */
+	delete[] items; /* free memory */
 	items = new_items;
 }
-/* class declaration */
+
+/*
+ * class: Stack
+ * ------------
+ * Defines the public member functions that this
+ * class exports. 
+ */
 static const int kInitialSize = 100;
 template<typename T>
 class Stack {
@@ -77,7 +90,8 @@ class Stack {
 	private:
 		T *items;
 		int top; /* index of next top element [also serves as the count] */
-		int arr_size;
+		         /* if top == 0, the stack is empty */
+		int cap;
 };
 
 /* class definition */
@@ -91,12 +105,15 @@ class Stack {
  */
 template<typename T>
 Stack<T>::Stack() {
-	items = new int[kInitialSize];
+	items = new T[kInitialSize];
 	top = 0; /* initialize to empty */
-	arr_size = kInitialSize; /* initial size */
+	cap = kInitialSize; /* initial size */
 }
 
-/* Destructor */
+/* 
+ * Destructor 
+ * ----------
+ */
 template<typename T>
 Stack<T>::~Stack() {
 	delete[] items;
@@ -105,48 +122,95 @@ Stack<T>::~Stack() {
 /* 
  * Push 
  * ----
- * First increases the size if out of 
- * capacity. And then adds the element
- * to the top of the stack. 
+ * Adds an item to the top of the stack. Internally this places the item
+ * at the end of the array, which is denoted by the variable top. If the capacity
+ * of the internal array is exceeded we double the internal array capacity.
+ * We note that this operation takes constant amortized time. By amortized 
+ * we mean that the average cost over a sequence of push operations is 
+ * constant even though in the worst case a push operation might take time 
+ * proportional to n, where n is the maximum size of the internal array. 
+ * The amortized constant time is a consequence of the following fact: For 
+ * a push operation to cost O(n), it must have been the case that there were 
+ * n constant (O(1)) push operations that prededed it and filled up the array. 
+ * Consider the following illustration, where we start out with an empty internal 
+ * array of size 4.
+ *
+ *
+ * |__|__|__|__|
+ *
+ *
+ * Now observe the state of the array after four push operations:
+ *
+ * operation       cost
+ * ---------       ----
+ * push(4);        O(1)
+ * push(5);        O(1)
+ * push(8);        O(1)
+ * push(2);        O(1)
+ *
+ * |_4_|_5_|_8_|_2_|
+ *
+ * The next push operation will require a doubling of the internal array prior
+ * to the push which takes constant time. We know the doubling will take time
+ * proportional to the capacity of the array (in this case 4). Thus we have
  * 
- * Run Time: 
- *  The push operation is a simple 
- *  insert operation that takes a 
- *  constant O(1) amortized time.
+ * operation       cost
+ * ---------       ----
+ * push(3)         O(n) [where n is the array capacity]
+ *
+ *
+ * But considering the general case entire sequence of operations, push(4) 
+ * through push(3) we have the following average over the first n + 1 pushes:
+ *
+ * [O(1) + O(1) + ... + O(1) + O(n)] / (n + 1) = O(1)
+ *
+ * This is what we mean when we say push has a constant amortized cost.
  */
 template<typename T>
 void Stack<T>::push(T a) {
-	/* check if there is space */
-	int nxt_indx = top + 1;
-	if(nxt_indx == (arr_size - 1)) {
+	/* check if there is space (i.e. whether top + 1 is capacity, since top is 0-indexed) */
+	if((top + 1) == cap) {
 		/* increase the array capacity */
-		increase_capacity(items,arr_size);
-		arr_size *= 2;
+		resize(items,cap, 2 * cap);
+		cap *= 2;
 	}
-
 	/* add to new position */
-	items[++top] = a;
+	items[top++] = a;
 }
 
 /*
  * Pop
  * ---
- * Pops an element from the top of the 
- * stack if there is an element, otherwise
- * returns null.
+ * This operation allows the cient to pop an element from the top of the stack.
+ * This is internally represented by decrementing the value of our variable
+ * top, which is the index of the top most element in the array. Since a sequence
+ * of pop operations can result in a sparsely populated array, this operation 
+ * halves the internal array capacity when it is not being adequately utilized 
+ * (i.e. it's 1/4 full.). The pop operation takes constant amortized 
+ * time, meaning the halving of the array occurs very infrequently compared to 
+ * pop operations that take O(1) time. This operation also throws an exception 
+ * if pop is called on an empty stack.
  *
- * Run Time: 
- *  The pop operation is a simple 
- *  insert operation that takes a 
- *  constant O(1) amortized time.
- *  What the fuck does that mean?
+ * Note that our choice of halving the array when it is a fourth of its initial 
+ * capacity is not arbitrary. If you think about it for a second, if we halved
+ * the array when it was 'half' its capacity, halving it would essentially 
+ * leave the resulting array full. Thus, by halving the array when it is a 
+ * half full, we can accomodate a number of push and pop operations in the 
+ * resulting array before having to resize again.
  */
  template <typename T>
  T Stack<T>::pop() {
- 	if(!this->empty()) {
- 		return items[top--];
+ 	if(this->empty()) {
+ 		throw "Stack Is Empty";
+ 	} else {
+ 		/* check for resize */
+ 		/* note the number of items in the internal array is precisely 'top' */
+ 		if(top <= cap/4) {
+ 			resize(items,cap,cap/2);
+ 			cap = cap/2;
+ 		}
+ 		return items[--top];
  	}
- 	throw "Stack Is Empty";
  }
 
  /*
@@ -157,6 +221,6 @@ void Stack<T>::push(T a) {
   */
   template <typename T>
   bool Stack<T>::empty() {
-  	return top == -1;
+  	return top == 0;
   }
 #endif
--- stack/stacktest.cc
+++ stack/stacktest.cc
@@ -1,30 +1,41 @@
 //#include "ar-stack.h"
 #include <iostream>
 #include "ar-stack.h"
+#include <string>
 using namespace std;
 
 void basic_test1() {
 	cout << "basic tests 1 " << endl;
-	Stack<int> s;
-	s.push(1);
-	s.push(2);
-	s.push(3);
+	Stack<int> *s = new Stack<int>;
+	s->push(1);
+	s->push(2);
+	s->push(3);
 	for(int i = 0; i < 3; i++) {
-		cout << s.pop() << endl;
+		cout << s->pop() << endl;
 	}
 }
 
 void basic_test2() {
 	cout << "basic tests 2 " << endl;
-	Stack<int> s;
-	for(int i = 0; i < 200; i++) {
-		s.push(i);
-		cout << s.pop() << endl;
+	Stack<double> *s = new Stack<double>;
+	for(double i = 0.0; i < 200.0; i++) {
+		s->push(i);
+		cout << s->pop() << endl;
 	}
+}
+
+void string_test() {
+  Stack<string> s;
+  s.push("Eliezer");
+  s.push("Abate");
+  for(int i = 0; i < 2; i++) {
+    cout << s.pop() << endl;
+  }
 }
 
 int main() {
   basic_test1();
   basic_test2();
+  string_test();
   return 0;
 }
